da9f3a3a2cb9e08be7168e4ec56df7327320d2c8
memorialchurchanglican <Church@86>
2018-05-12 20:46:17 +0530

    updating gallery for CS 2017 and Treasure Hunt 2018

--- anchor_music.py
+++ anchor_music.py
@@ -1,8 +1,8 @@
 import os
 
-for file in os.listdir('Pew Slip'):
-    print  ("<li><a href=\"Pew Slip\\" + file + "\" alt=\"" + file + "\" target=\"_blank\">" + file + "</a></li>")
+# for file in os.listdir('Pew Slip'):
+#     print  ("<li><a href=\"Pew Slip\\" + file + "\" alt=\"" + file + "\" target=\"_blank\">" + file + "</a></li>")
 
 
-
-
+for file in os.listdir('assets/images/Carol-Service-2017/'):
+    print('<img src=\"assets/images/Carol-Service-2017/' + file + "\"  " + 'style="width:100%">')
